c7ac9a864a5bf148889bb550d7ea39e6806771dc
Mads25O <mo.overgaard@live.dk>
2026-04-05 14:21:23 +0200

    Make shell customization its own file

--- modules/hosts/desktop-machine/_home.nix
+++ modules/hosts/desktop-machine/_home.nix
@@ -4,65 +4,9 @@
   home.homeDirectory = "/home/mads";
   home.stateVersion = "25.11";
 
+  imports = [ ../../features/_shell.nix ];
+
   services.cliphist.enable = true;
-
-  programs.bash = {
-    enable = true;
-    initExtra = ''
-      eval "$(starship init bash)"
-    '';
-  };
-
-  programs.starship = {
-    enable = true;
-    settings = {
-      format = "$directory$git_branch$git_status$python$character";
-      character = {
-        success_symbol = "[❯](purple)";
-        error_symbol = "[❯](red)";
-      };
-      directory = {
-        style = "blue";
-        truncation_length = 3;
-        truncate_to_repo = false;
-      };
-      git_branch = {
-        symbol = " ";
-        style = "green";
-      };
-      git_status = {
-        style = "yellow";
-      };
-
-      python = {
-        format = "[$virtualenv]($style) ";
-        style = "yellow";
-        python_binary = "python3";
-      };
-    };
-  };
-
-  programs.kitty = {
-    enable = true;
-    settings = {
-      font_family = "JetBrainsMonoNL Nerd Font";
-      font_size = "13.0";
-      background_opacity = "0.95";
-      enable_audio_bell = false;
-      cursor_shape = "beam";
-      scrollback_lines = 10000;
-      background = "#1e1e2e";
-      foreground = "#cdd6f4";
-      color0 = "#45475a";
-      color1 = "#f38ba8";
-      color2 = "#a6e3a1";
-      color3 = "#f9e2af";
-      color4 = "#89b4fa";
-      color5 = "#f5c2e7";
-      color6 = "#94e2d5";
-      color7 = "#bac2de";
-    };
-  };
 
   programs.swaylock = {
     enable = true;
--- modules/hosts/laptop-machine/_home.nix
+++ modules/hosts/laptop-machine/_home.nix
@@ -4,58 +4,9 @@
   home.homeDirectory = "/home/mads";
   home.stateVersion = "25.11";
 
-  programs.bash = {
-    enable = true;
-    initExtra = ''
-      eval "$(starship init bash)"
-    '';
-  };
+  imports = [ ../../features/_shell.nix ];
 
-  programs.starship = {
-    enable = true;
-    settings = {
-      format = "$directory$git_branch$git_status$character";
-      character = {
-        success_symbol = "[❯](purple)";
-        error_symbol = "[❯](red)";
-      };
-      directory = {
-        style = "blue";
-        truncation_length = 3;
-        truncate_to_repo = false;
-      };
-      git_branch = {
-        symbol = " ";
-        style = "green";
-      };
-      git_status = {
-        style = "yellow";
-      };
-    };
-  };
-
-  programs.kitty = {
-    enable = true;
-    settings = {
-      font_family = "JetBrainsMonoNL Nerd Font";
-      font_size = "13.0";
-      hide_window_decoration = "yes";
-      background_opacity = "0.95";
-      enable_audio_bell = false;
-      cursor_shape = "beam";
-      scrollback_lines = 10000;
-      background = "#1e1e2e";
-      foreground = "#cdd6f4";
-      color0 = "#45475a";
-      color1 = "#f38ba8";
-      color2 = "#a6e3a1";
-      color3 = "#f9e2af";
-      color4 = "#89b4fa";
-      color5 = "#f5c2e7";
-      color6 = "#94e2d5";
-      color7 = "#bac2de";
-    };
-  };
+  services.cliphist.enable = true;
 
   programs.swaylock = {
     enable = true;
